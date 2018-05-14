ed59dcdf2ca568ebb4b1413aab097c6200493fce
KazuhiroObama <obamakazuhiro@gmail.com>
2018-05-14 15:07:32 +0900

    aaa

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_url, notice: "#{favorite.user.name}さんのブログをお気に入り登録しました"
+    redirect_to blogs_url, notice: "#{User.find(Blog.find(params[:blog_id]).user_id).name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(blog_id: params[:blog_id]).destroy
-    redirect_to blogs_url, notice: "#{favorite.user.name}さんのブログをお気に入り解除しました"
+    redirect_to blogs_url, notice: "#{User.find(Blog.find(params[:blog_id]).user_id).name}さんのブログをお気に入り解除しました"
   end
 end
